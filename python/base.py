dc80ec28fc1ee3a9c6787851d294b88176a8beef
blueplanet <erguolinge@gmail.com>
2017-01-07 11:47:23 +0900

    list? proc?

--- python/base.py
+++ python/base.py
@@ -2,3 +2,8 @@
 dic = { 'b': 'bbb', 2: 20, 1: 10, 0: 0, 'a': 'aaa' }
 for key in dic:
     print key, dic[key]
+
+# list?
+array = [1, 2, 3, 4, 5]
+myList = [item**2 for item in array]
+print myList

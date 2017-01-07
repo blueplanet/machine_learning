93358cf3c4a1f0aaaf2dd4e0eedea7cfaedb289e
blueplanet <erguolinge@gmail.com>
2017-01-07 14:09:48 +0900

    mat index

--- python/mat.py
+++ python/mat.py
@@ -20,3 +20,21 @@ print shape(mm.T)
 print shape(ss * mm.T)
 
 print multiply(mm, ss)
+
+mm = mat([2, 3, 1])
+mm.sort()
+print mm
+
+dd = mat([4, 5, 1])
+print dd.argsort()
+print dd
+print 'dd.mean()', dd.mean()
+
+jj = mat([[1, 2, 3], [8, 8, 8]])
+print 'shape(jj)', shape(jj)
+print 'jj[0, :]', jj[0, :]
+print 'jj[1, :]', jj[1, :]
+print 'jj[:, 0]', jj[:, 0]
+print 'jj[:, 1]', jj[:, 1]
+
+print 'jj[0, 0:2]', jj[0, 0:3]

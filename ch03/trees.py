295733232be8660ff65f2f4ed6e91c0435bdfcd4
blueplanet <erguolinge@gmail.com>
2017-01-11 04:51:41 +0900

    create tree

--- ch03/trees.py
+++ ch03/trees.py
@@ -69,3 +69,23 @@ def majorityCnt(classList):
 
     sortedClassCount = sorted(classCount.iteriitems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueValues = set(featValues)
+    for value in uniqueValues:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+
+    return myTree

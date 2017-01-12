00d679d9a837e47708b828ac3e73d0fb007a04b4
blueplanet <erguolinge@gmail.com>
2017-01-13 06:47:49 +0900

    trainNB0

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -1,3 +1,5 @@
+from numpy import *
+
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                     ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -23,3 +25,25 @@ def setOfWords2Vec(vocabList, inputSet):
         else: print "the word: %s is not in my Vocabulary!" % word
 
     return returnVec
+
+def trainNB0(trainMat, trainCategory):
+    numTrainDocs = len(trainMat)
+    numWords = len(trainMat[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMat[i]
+            p1Denom += sum(trainMat[i])
+        else:
+            p0Num += trainMat[i]
+            p0Denom += sum(trainMat[i])
+
+    p1Vect = p1Num / p1Denom
+    p0Vect = p0Num / p0Denom
+
+    return p0Vect, p1Vect, pAbusive

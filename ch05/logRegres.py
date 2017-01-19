d01f4a3e3d43c55ce29d43fabb080e7f9994f803
blueplanet <erguolinge@gmail.com>
2017-01-20 06:15:50 +0900

    multi test

--- ch05/logRegres.py
+++ ch05/logRegres.py
@@ -101,7 +101,7 @@ def classifyVector(inX, weights):
 
 def colicTest():
     frTrain = open('horseColicTraining.txt')
-    frTest = open('horseColiTest.txt')
+    frTest = open('horseColicTest.txt')
     trainingSet = []
     trainingLables = []
 
@@ -111,3 +111,27 @@ def colicTest():
         # TODO: lineArr = [float(value) for value in currLine[:21]]
         for i in range(21):
             lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLables.append(float(currLine[21]))
+
+    trainWeights = stocGradAscent1(array(trainingSet, trainingLables, 500))
+    errorCount = 0
+    numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print 'the error rate of this test is: %f' % errorRate
+    return errorRate
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print 'after %d iterations the aveage error rate is: %f' % (numTests, errorSum / float(numTests))

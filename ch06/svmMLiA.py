2025a653c3c460c2976dbf685032dc9c7701fff9
blueplanet <erguolinge@gmail.com>
2017-02-28 05:35:20 +0900

    test digits

--- ch06/svmMLiA.py
+++ ch06/svmMLiA.py
@@ -186,4 +186,57 @@ def testRbf(k1 = 1.3):
         if sign(predict) != sign(labelArr[i]): errorCount += 1
     print "the test error rate is: %f" % (float(errorCount) / m)
 
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    file = open(filename)
+
+    for i in range(32):
+        line = file.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(line[j])
+
+    return returnVect
+
+def loadImages(dirName):
+    from os import listdir
+    hwLabels = []
+    trainingFileList = listdir(dirName)
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        filenameStr = trainingFileList[i]
+        fileStr = filenameStr.split('.')[0]
+        if classNumStr == 9: hwLabels.append(-1)
+        else: hwLabels.append(1)
+        trainingMat[i, :] = img2vector('%s/%s' % (dirName, filenameStr))
+    return trainingMat, hwLabels
+
+def testDigits(kTup = ('rbf'), 10)):
+    dataArr, labelArr = loadImages('trainingDigits')
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, kTup)
+    datMat = mat(dataArr); labelMat = mat(labelArr).transpose()
+    svInd = nonzero(alphas.A > 0)[0]
+    sVs = datMat[svInd]
+    labelSV = labelMat[svInd]
+    print "there are %d Support Vectors" % shape(sVs)[0]
+    m, n = shape(datMat)
+
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i, :], kTup)
+        predict = kernelEval..T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]): errorCount += 1
+
+    print "the training error rate is: %f" % (float(errorCount) / m)
+    dataArr, labelArr = loadImages('testDigits')
+    errorCount = 0
+    datMat = mat(dataArr); labelMat = mat(labelArr).transpose()
+    m, n = shape(datMat)
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i, :], kTup)
+        predict = kernelEval.T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]): errorCount += 1
+
+    print "the test error rate is: %f" % (float(errorCount) / m)
+
 
